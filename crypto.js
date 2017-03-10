0260769f022871233a8ccd0b16acb89c2389bda8
Ben Hoffman <dbenhoffman@gmail.com>
2017-03-10 16:25:46 -0700

    gerp

--- crypto.js
+++ crypto.js
@@ -1,18 +1,21 @@
 const nacl = require('tweetnacl')
 const util = require('tweetnacl-util')
 
-const crypto = (pub, priv) => ({
-  sign: (object = null) => {
-    const binObject = util.decodeUTF8(JSON.stringify(object))
-    const hash = nacl.hash(binObject)
-    const sig = nacl.sign.detached(hash)
-    return Object.assign({}, object, {
-      signedBy: {
-        publicKey: pub,
-        signature: util.encodeBase64(sig)
-      }
-    })
+const crypto = (pub, priv) => {
+  const privBin = util.decodeBase64(priv)
+  return {
+    sign: (object = null) => {
+      const binObject = util.decodeUTF8(JSON.stringify(object))
+      const hash = nacl.hash(binObject)
+      const sig = nacl.sign.detached(hash, privBin)
+      return Object.assign({}, object, {
+        signedBy: {
+          publicKey: pub,
+          signature: util.encodeBase64(sig)
+        }
+      })
+    }
   }
-})
+}
 
-export default crypto
+module.exports = crypto
--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const task = require('task.js')
 
-const data = [...Array(200)].map((_, i) => ({ value: `this is value ${i}` }))
+const data = [...Array(100)].map((_, i) => ({ value: `this is value ${i}` }))
 
-console.log(data)
+const publicKey = 'XvD6bhtdxC2FVBXVzUNanqXoNSoB+O6XsUdZJKoZtdE='
+const privateKey = 'M2aqJGwGNAk3gRyy/UQ09RF2Segea821qXzWgLv4b5Ze8PpuG13ELYVUFdXNQ1qepeg1KgH47pexR1kkqhm10Q=='
+
+const cryptoBun = require('./crypto.bundled')
+console.log(cryptoBun)
+
+const signTask = task.defaults({
+  warmStart: true,
+  maxWorkers: 11,
+  globals: {
+    publicKey,
+    privateKey,
+    __dirname
+  },
+  initialize: globals => {
+    globals.crypto = require(globals.__dirname + '/crypto')(globals.publicKey, globals.privateKey)
+    return globals
+  }
+}).wrap(object => {
+  return globals.crypto.sign(object)
+})
+
+data.forEach(obj => signTask(obj).then(console.log.bind(console)))
